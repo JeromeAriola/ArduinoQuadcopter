9e0fbe7d6c287260dd9aa547cb6c2828e57f06f7
JeromeAriola <jeromeariola9@gmail.com>
2017-04-08 10:52:53 +1000

    Add files via upload
    Just a little code I wrote to test your potentiometers and to find the analog values (ie 1023 is 5v). The pins on the pot are connected to the Arduino via female jumper wires. The middle is for any analog pin of your choice, and the left and right pins are for ground and 5 volts, although it doesn't matter ;)

---
+++ Potentiometer_Test.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+int readPot(){
+  return analogRead(14);
+  }
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(readPot() <= 1023){
+    Serial.print("Something...");
+    delay(100);
+    
+    }else if(readPot() == 0 ){
+      Serial.println("Nothing...");
+      delay(100);
+      }
+  Serial.print(readPot());
+}
+
