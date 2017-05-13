85b530e18184aabbd58261d4a29b1602a7b48764
JeromeAriola <jeromeariola9@gmail.com>
2017-05-13 18:18:52 +1000

    Update Quadcopter_Source_Code_Main_Transmitter.ino

--- Quadcopter_Source_Code_Main_Transmitter.ino
+++ Quadcopter_Source_Code_Main_Transmitter.ino
@@ -1,117 +1,124 @@
 /*
 Transmitter Code for TX
+
+
+Potentiometer Setup
+   ____________
+  |  --------  |
+  |  |      |  |---
+  |  |      |  |--- Y
+  |  --------  |---
+  |____________|
+      |  |  |
+        X
 */
 
-//I should have functions where it automatically sends if potentiometer
-//values are what they are I tell them to be (i.e. up on the potentiometer, 
-//then the up() function will execute sending the "U" string)
-
-#include <stdio.h>
 #include <printf.h>
 #include <nRF24L01.h>
 #include <RF24_config.h>
 #include <RF24.h>
+#include <SPI.h>
 
-//Left Potentiometer Shortcuts in Code
-#define up /*leftPotentiometer up voltage value*/           //or maybe I can switch
-#define down /*leftPotentiometer down voltage value*/       //for a const int
-#define left /*leftPotentiometer left voltage value*/
-#define right /*leftPotentiometer right voltage value*/
-
-#define up /*rightPotentiometer up voltage value*/
-#define down /*rightPotentiometer down voltage value*/
-#define left /*rightPotentiometer left voltage value*/
-#define right /*rightPotentiometer right voltage value*/
+//pot values for direction -- pls dont forget to append after #define
+#define center 512
+#define up 1023
+#define down 0
+#define left 1023
+#define right 0
  
 #define CE_PIN   14
 #define CSN_PIN 15
 
-const int leftPot = 14; //A0
-const int rightPot = 15; //A1
+const int leftPotX = 14; //A0
+const int leftPotY = 15; //A1
+
+const int rightPotX = 16; //A2
+const int rightPotY = 17; //A3
 
 const byte address[6] = "00001";
 
-char rollForward[3] = "RF";
-char rollBackward[3] = "RB"; 
-char leftYaw[3] = "YL";
-char rightYaw[3] = "YR";
+RF24 radio(CE_PIN, CSN_PIN);
 
-char rollRight[3] = "RR";
-char rollLeft[3] = "RL";
-char up[3] = 'U'
-char down[3] = 'D'
+char command;
 
-RF24 transmitter(CE_PIN, CSN_PIN);
+void sendCommand()
+{
+  radio.write(&command, sizeof(command));
+}
 
-//Left Potentiometer Commands
-char rollForward = "RF"
-char rollBack = "RB"
-char yawLeft = "YL"
-char yawRight = "YR"
+int readLeftPotX()
+{
+  return analogRead(leftPotX);
+}
 
-//Right Potentiometer Commands
-char altitudeUp = 'U'
-char altitudeDown = 'D'
-char rollLeft = "RL"
-char rollRight = "RR"
+int readLeftPotY()
+{
+  return analogRead(leftPotY);
+}
 
+int readRightPotX()
+{
+  return analogRead(rightPotX);
+}
 
+int readRightPotY()
+{
+  return analogRead(rightPotY);
+}
 
-void setup(){
-  pinMode(leftPot, INPUT);
-  pinMode(rightPot, INPUT);
+void setup()
+{
+  pinMode(leftPotX, INPUT);
+  pinMode(leftPotY, INPUT);
+  pinMode(rightPotX, INPUT);
+  pinMode(rightPotY, INPUT);
 
   radio.begin();
   radio.setPayloadSize(8);
-  radio.openWritingPipe(0, address);
   radio.setRetries(15, 15);
+  radio.openWritingPipe(address);
 }
 
-int readLeftPot(){
-  return analogRead(leftPot);
+void loop()
+{
+  if(readLeftPotX() || readLeftPotY() || readRightPotX() ||readRightPotY() == 512)
+  {
+    command = 'H';
+  }
+  //Left Potentiometer Commands 
+  if(readLeftPotX() == left)
+  {
+    command = 'RL';
+  } 
+  if(readLeftPotX() == right)
+  {
+    command = 'RR';
+  }
+  if(readLeftPotY() == up)
+  {
+    command = 'RF';
+  }
+    if(readLeftPotY() == down)
+  {
+    command = 'RB';
+  }
+
+  //Right Potentiometer Commands
+  if(readRightPotX() == left)
+  {
+    command = 'YL';
+  }
+    if(readRightPotX() == right)
+  {
+    command = 'YR';
+  }  
+  if(readRightPotY() == up)
+  {
+    command = 'U';
+  }  
+  if(readRightPotY() == down)
+  {
+    command = 'D';
+  }
+  sendCommand();
 }
-
-int readRightPot(){
-  return analogRead(rightPot);
-}
-
-void loop(){
-  int leftPotValue = readLeftPot();
-  int rightPotValue =  readRightPot();
-
-//Right Potentiometer Conditionals  
-  if(rightPotValue == /*pot value for up*/){
-    /*send a message U in serial monitor */
-    }
-
-  if(rightPotValue == /*pot value for down*/){
-    /*serialWrite D*/
-    /*send the message via radio and serial monitor*/
-    }
-
-  if(rightPotValue == /*pot value for left roll*/){
-    /*serialWrite RL*/
-    /*send the message via radio and serial monitor*/
-    }
-  if(rightPotValue == /*pot value for right roll*/){
-    /*serialWrite RR*/
-    }
-
-//Left Potentiometer Conditionals
-  if(leftPotValue == /*pot value for forward roll*/){
-    /*send the message RF*/
-    }
-    
-  if(leftPotValue == /*pot value for back roll*/){
-    /*send the message RB*/
-    }
-
-  if(leftPotValue == /*pot value for left yaw*/){
-    /*send the message YL*/
-    }
-    
-  if(leftPotValue == /*pot value for right yaw*/){
-    /*send the message YR*/
-    }
-}
-
