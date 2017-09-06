f9df9c6bf14e9441569b70416493a4831b14e418
JeromeAriola <jeromeariola9@gmail.com>
2017-09-06 11:15:30 +1000

    Revision of code -- works better now!

--- Potentiometer_Test.ino
+++ Potentiometer_Test.ino
@@ -3,20 +3,34 @@ void setup() {
 Serial.begin(9600);
 }
 
-int readPot(){
-  return analogRead(14);
+
+int readPot2(){
+  return analogRead(18);
+  }
+
+int readPot1(){
+  return analogRead(19);
   }
   
 void loop() {
   // put your main code here, to run repeatedly:
-  if(readPot() <= 1023){
-    Serial.print("Something...");
-    delay(100);
+  if(readPot1() <= 1023){
+    Serial.println("Y");
+    delay(500);
     
-    }else if(readPot() == 0 ){
-      Serial.println("Nothing...");
-      delay(100);
+    }else if(readPot1() == 0 ){
+      Serial.println("Y");
+      delay( 500);
       }
-  Serial.print(readPot());
-}
+  Serial.print(readPot1());
 
+    if(readPot2() <= 1023){
+    Serial.println("X");
+    delay( 500);
+    
+    }else if(readPot2() == 0 ){
+      Serial.println("X");
+      delay( 500);
+      }
+  Serial.print(readPot2());
+}
